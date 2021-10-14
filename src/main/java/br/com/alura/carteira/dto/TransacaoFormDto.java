3ee03888dd9469f476022667cd24871925cf5ac1
Antonio Eloy <antonioeloy14@gmail.com>
2021-10-14 09:01:15 -0300

    Tratamento de erros HTTP 400 e 500

--- src/main/java/br/com/alura/carteira/dto/TransacaoFormDto.java
+++ src/main/java/br/com/alura/carteira/dto/TransacaoFormDto.java
@@ -3,6 +3,7 @@ package br.com.alura.carteira.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -10,6 +11,7 @@ import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.com.alura.carteira.modelo.TipoTransacao;
@@ -22,16 +24,18 @@ public class TransacaoFormDto {
 
 	@NotBlank
 	@Size(min = 5, max = 6)
-	@Pattern(regexp = "[a-zA-z]{4}[0-9][0-9]?")
+	@Pattern(regexp = "[a-zA-z]{4}[0-9][0-9]?", message = "{transacao.ticker.invalido}")
 	private String ticker;
 	
 	@NotNull
+	@DecimalMin(value = "0.01")
 	private BigDecimal preco;
 	
 	@NotNull
-	@Min(1)
+	@Min(value = 1)
 	private Integer quantidade;
 	
+	@NotNull
 	@PastOrPresent
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate data;
@@ -40,6 +44,7 @@ public class TransacaoFormDto {
 	private TipoTransacao tipo;
 	
 	@NotNull
+	@JsonAlias("usuario_id")
 	private Long usuarioId;
 
 }
