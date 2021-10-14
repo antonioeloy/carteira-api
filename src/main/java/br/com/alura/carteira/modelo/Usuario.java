f575f996381fc7f992a1ce546370258f28f00b7b
Antonio Eloy <antonioeloy14@gmail.com>
2021-10-14 20:46:48 -0300

    Adicionando testes do TransacaoRepository

--- src/main/java/br/com/alura/carteira/modelo/Usuario.java
+++ src/main/java/br/com/alura/carteira/modelo/Usuario.java
@@ -32,4 +32,10 @@ public class Usuario {
 	@Exclude
 	private String senha;
 
+	public Usuario(String nome, String login, String senha) {
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
 }
