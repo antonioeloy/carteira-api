d0eef9a1d820636889bf458429d8a43e040206e0
Antonio Eloy <antonioeloy14@gmail.com>
2021-10-23 21:33:45 -0300

    Complementando testes de integração da classe UsuarioController

--- src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
@@ -1,5 +1,6 @@
 package br.com.alura.carteira.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(SpringExtension.class)
@@ -25,23 +27,6 @@ class UsuarioControllerTest {
 	
 	@Autowired
 	private MockMvc mvc;
-
-	@Test
-	void naoDeveriaCadastrarUsuarioComDadosIncompletos() throws Exception {
-		
-		String json = "{}";
-		
-		mvc
-		.perform(
-				post("/usuarios")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(json)
-				)
-		.andExpect(
-				status().isBadRequest()
-				);			
-		
-	}
 	
 	@Test
 	void deveriaCadastrarUsuarioComDadosCompletos() throws Exception {
@@ -64,6 +49,97 @@ class UsuarioControllerTest {
 				content().json(json)
 				);
 		
+	}
+
+	@Test
+	void naoDeveriaCadastrarUsuarioComDadosIncompletos() throws Exception {
+		
+		String json = "{}";
+		
+		mvc
+		.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json)
+				)
+		.andExpect(
+				status().isBadRequest()
+				);			
+		
+	}
+	
+	@Test
+	void naoDeveriaCadastrarUsuarioComLoginJaEmUso() throws Exception {
+		
+		String json1 = "{\"nome\": \"Antonio Eloy\", \"login\": \"antonio.eloy@email.com.br\"}";
+		
+		mvc
+		.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json1)
+				)
+		.andExpect(
+				status().isCreated()
+				)
+		.andExpect(
+				header().exists("Location")
+				)
+		.andExpect(
+				content().json(json1)
+				);
+		
+		String json2 = "{\"nome\": \"Antonio Eloy de Oliveira Araujo\", \"login\": \"antonio.eloy@email.com.br\"}";
+		
+		mvc
+		.perform(
+				post("/usuarios")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json2)
+				)
+		.andExpect(
+				status().isBadRequest()
+				);
+		
+	}
+	
+	@Test
+	void deveriaRetornarUmUsuario() throws Exception {
+		
+		String json = "{\"nome\": \"Antonio Eloy\", \"login\": \"antonio.eloy@email.com.br\"}";
+		
+		MvcResult resultadoPostUsuario = mvc
+				.perform(
+						post("/usuarios")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json)
+						)
+				.andExpect(
+						status().isCreated()
+						)
+				.andExpect(
+						header().exists("Location")
+						)
+				.andExpect(
+						content().json(json)
+						)
+				.andReturn();
+		
+		String location = resultadoPostUsuario.getResponse().getHeader("Location");
+		
+		String idUsuario = location.substring(location.lastIndexOf("/") + 1);
+		
+		mvc
+		.perform(
+				get("/usuarios/" + idUsuario)
+				)
+		.andExpect(
+				status().isOk()
+				)
+		.andExpect(
+				content().json(json)
+				);
+				
 	}
 
 }
